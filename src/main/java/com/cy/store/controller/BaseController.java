71c7b9296da65fe3a76fc64eb67d879cf42a8f66
yk838 <1052018251@qq.com>
2022-03-28 20:57:36 +0800

    增加baseController controller异常状态码

--- src/main/java/com/cy/store/controller/BaseController.java
+++ src/main/java/com/cy/store/controller/BaseController.java
@@ -5,6 +5,7 @@ package com.cy.store.controller;
  * @date 2022/3/14  19:22
  */
 
+import com.cy.store.controller.ex.*;
 import com.cy.store.service.ex.*;
 import com.cy.store.util.JsonResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,7 +20,7 @@ public class BaseController {
     // 请求处理方法，这个方法的返回值就是需要传递给前端的数据
     // 自动将异常对象传递此方法的参数列表上
     // 当前项目中产生的异常，统一拦截在方法上，这个方法此时就冲当的是请求处理方法，方法的返回值直接给前端
-    @ExceptionHandler(ServiceException.class) //用于统一抛出异常
+    @ExceptionHandler({ServiceException.class, FileUploadException.class}) //用于统一抛出异常
     public JsonResult<Void> handleException(Throwable e){
         JsonResult<Void> result = new JsonResult<>(e);
         if ( e instanceof UsernameDuplicateException) {
@@ -34,7 +35,17 @@ public class BaseController {
         }else if ( e instanceof InsertException) {
             result.setState(5000);
             result.setMessage("用户注册过程中产生的位置异常");
-        }
+        }  else if (e instanceof FileEmptyException) {
+        result.setState(6000);
+        } else if (e instanceof FileSizeException) {
+            result.setState(6001);
+        } else if (e instanceof FileTypeException) {
+            result.setState(6002);
+        } else if (e instanceof FileStateException) {
+            result.setState(6003);
+        } else if (e instanceof FileUploadIOException) {
+            result.setState(6004);
+    }
         return  result;
     }
 
